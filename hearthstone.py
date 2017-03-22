7ca2bafd7225548312b17979681901b74e6c2e7c
Gordon <thisisgordonsemail@gmail.com>
2017-03-21 22:40:11 -0700

    Added support for comedy cards.

--- hearthstone.py
+++ hearthstone.py
@@ -13,18 +13,42 @@ from config import mashape_key
 
 hearth_rx = re.compile('\[(.+?)\]')
 
+# Bot gibberish
+thank_yous = ['thank you', 'god bless', 'bless you', 'much  obliged', 'thanks', 'thx', 'gracias', 'merci', 'danke', 'much obliged']
+try_agains = ['try more letters', 'type smarter words', 'how about a better set of words', 'try again', 'don\'t you look silly']
+
+# Comedy cards
+# Not actual cards but I want the bot to respond as if it was, jokes you see.
+comedy_cards = {'blood manos' : {'title': 'Blood Manos', 'url': 'http://i.imgur.com/a5fXYW4.png', 'source': 'http://imgur.com/a/IEUSR', 'color': 'orange'} }
+
 def trigger(message):
     query = re.search(hearth_rx, message.content.lower().strip())
     if query and query.groups()[0].strip():
         return True
 
 async def action(message, client):
+    # Get the search query from inside the []
     query = re.search(hearth_rx, message.content.lower().strip())
-    results = hearthstone_card_search(query.groups()[0].strip())
-    cards_to_remove = []
+    query = query.groups()[0].strip()
+
+    if query in comedy_cards.keys():
+        embed_message = Embed(title = comedy_cards[query]['title'],
+                              type = 'rich',
+                              url = comedy_cards[query]['source'])
+        embed_message.set_image(url = comedy_cards[query]['url'])
+        embed_message.color = _set_color(comedy_cards[query]['color'])
+        await client.send_message(message.channel, embed = embed_message)
+
+    # Run the search
+    results = hearthstone_card_search(query)
+
     if results:
+        cards_to_remove = []
         for card in results:
-            if card['type'] not in ('Minion', 'Spell', 'Weapon') or card['cardSet'] in ('Debug', 'Tavern Brawl') or card['name'] in ('Jade Golem') or 'flavor' not in card.keys():
+            if card['type'] not in ('Minion', 'Spell', 'Weapon') \
+                    or card['cardSet'] in ('Debug', 'Tavern Brawl')  \
+                    or card['name'] in ('Jade Golem') \
+                    or 'flavor' not in card.keys():
                 cards_to_remove.append(card)
 
         for card in cards_to_remove:
@@ -37,7 +61,7 @@ async def action(message, client):
                                   url = 'http://www.hearthpwn.com/search?search={0}#t1:cards'.format(urllib.parse.quote_plus(card['name'])))
             embed_message.set_footer(text = card['flavor'])
             embed_message.set_image(url = _random_gold(card))
-            embed_message.color = _set_color(card)
+            embed_message.color = _set_color(card['rarity'])
             await client.send_message(message.channel, embed = embed_message)
 
         elif len(results) > 1 and len(results) < 10:
@@ -48,13 +72,13 @@ async def action(message, client):
                                           url = 'http://www.hearthpwn.com/search?search={0}#t1:cards'.format(urllib.parse.quote_plus(result_card['name'])))
                     embed_message.set_footer(text = result_card['flavor'])
                     embed_message.set_image(url = _random_gold(result_card))
-                    embed_message.color = _set_color(result_card)
+                    embed_message.color = _set_color(result_card['rarity'])
                     await client.send_message(message.channel, embed = embed_message)
                     return
             msg = 'Multiple results found: *{0}*'.format(", ".join([x['name'] for x in results]))
             await client.send_message(message.channel, msg)
         elif len(results) >= 10:
-            msg = '{0} results found, {2}, {1}.'.format(len(results), random.choice(['thank you', 'god bless', 'bless you', 'much  obliged', 'thanks', 'thx', 'gracias', 'merci', 'danke', 'much obliged']), random.choice(['try more letters', 'type smarter words', 'how about a better set of words', 'try again', 'don\'t you look silly']))
+            msg = '{0} results found, {2}, {1}.'.format(len(results), random.choice(thank_yous), random.choice(try_agains))
             await client.send_message(message.channel, msg)
 
 def hearthstone_card_search(q):
@@ -74,15 +98,15 @@ def _random_gold(card):
     return image
 
 def _set_color(card):
-    if card['rarity'] == 'Free':
+    if card == 'Free' or card == 'light_grey':
         return Color.light_grey()
-    elif card['rarity'] == 'Common':
+    elif card == 'Common' or card == 'lighter_grey':
         return Color.lighter_grey()
-    elif card['rarity'] == 'Rare':
+    elif card == 'Rare' or card == 'blue':
         return Color.blue()
-    elif card['rarity'] == 'Epic':
+    elif card == 'Epic' or card == 'purple':
         return Color.purple()
-    elif card['rarity'] == 'Legendary':
+    elif card == 'Legendary' or card == 'orange':
         return Color.orange()
     else:
         return Color.default()
