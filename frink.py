38c67d48a41d08d16d535236db00dd526d85212e
Gordon <thisisgordonsemail@gmail.com>
2017-03-29 22:08:02 -0700

    Added better embeded messages

--- frink.py
+++ frink.py
@@ -12,16 +12,19 @@ def trigger(message):
 
 async def action(message, client):
     message.content = message.content.lower()
-    if message.content == "!simp":
-        random_screen = frinkiac.random()
-        embed_message = Embed(type = 'rich')
+
+    if message.content == "!fut" or message.content == "!simp":
+        simpsons_screen = True
+        if message.content == "!fut":
+            simpsons_screen = False
+
+        random_screen = frinkiac.random(simpsons_screen)
+        random_screen._get_details()
+        embed_message = Embed(type = 'rich', url = random_screen.rich_url, title = 'S{0}E{1} {2}'.format(random_screen.season, random_screen.ep_number, random_screen.ep_title))
         embed_message.set_image(url = random_screen.meme_url())
+        embed_message.set_footer(text = '{0}'.format(random_screen.wiki_link))
         await client.send_message(message.channel, embed = embed_message)
-    elif message.content == "!fut":
-        random_screen = frinkiac.random(False)
-        embed_message = Embed(type = 'rich')
-        embed_message.set_image(url = random_screen.meme_url())
-        await client.send_message(message.channel, embed = embed_message)
+    
     else:
         try:
             if message.content.startswith("!simp"):
@@ -31,13 +34,15 @@ async def action(message, client):
                 command, query = message.content.split("!fut ")
                 search = frinkiac.search(query, False)
             if search:
+                search[0]._get_details()
                 if query.startswith('"') and query.endswith('"'):
-                    embed_message = Embed(type = 'rich')
+                    embed_message = Embed(type = 'rich', url = search[0].rich_url, title = 'S{0}E{1} {2}'.format(search[0].season, search[0].ep_number, search[0].ep_title))
                     embed_message.set_image(url = search[0].meme_url(caption = query.split('"')[1]))
                     await client.send_message(message.channel, embed = embed_message)
                 else:
-                    embed_message = Embed(type = 'rich')
+                    embed_message = Embed(type = 'rich', url = search[0].rich_url, title = 'S{0}E{1} {2}'.format(search[0].season, search[0].ep_number, search[0].ep_title))
                     embed_message.set_image(url = search[0].meme_url())
+                    embed_message.set_footer(text = '{0}'.format(search[0].wiki_link))
                     await client.send_message(message.channel, embed = embed_message)
         except ValueError:
             await client.add_reaction(message, '?')
